08936a2186a3ca3621368a280a08b1e7c5ea863a
Tal Globus <talglobus@gmail.com>
2017-04-14 03:49:12 -0700

    Begin iOS app and web interface

---
+++ iOS/feelmap/feelmap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  feelmap
+//
+//  Created by Tal Globus on 4/14/17.
+//  Copyright © 2017 Tal Globus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
